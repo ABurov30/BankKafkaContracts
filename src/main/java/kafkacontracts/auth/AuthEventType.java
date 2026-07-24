d82848755605ca25cda222b03122fc0ea682b573
Andrey <andrey-valeriy.burov@mail.ru>
2026-07-24 20:44:03 +0300

    init

--- src/main/java/kafkacontracts/auth/AuthEventType.java
+++ src/main/java/kafkacontracts/auth/AuthEventType.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 public enum AuthEventType {
     AUTH_USER_CREATED(KafkaTopics.AUTH_USER_CREATED, "v1"),
     AUTH_USER_BLOCKED(KafkaTopics.AUTH_USER_BLOCKED, "v1"),
-    AUTH_USER_UNLOCK(KafkaTopics.AUTH_USER_UNLOCK, "v1");
+    AUTH_USER_UNLOCK(KafkaTopics.AUTH_USER_UNLOCK, "v1"),
+    AUTH_USER_VERIFIED(KafkaTopics.AUTH_USER_VERIFIED, "v1");
 
     private final String topic;
     private final String version;
