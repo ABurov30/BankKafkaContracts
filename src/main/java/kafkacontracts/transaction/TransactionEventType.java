dfe1d81e4ff0d9249fa0d7c922338ce97b4ed155
Andrey <andrey-valeriy.burov@mail.ru>
2026-08-11 13:23:19 +0300

    feat: move some transaction contracts to accoutn

--- src/main/java/kafkacontracts/transaction/TransactionEventType.java
+++ src/main/java/kafkacontracts/transaction/TransactionEventType.java
@@ -6,9 +6,7 @@ import lombok.Getter;
 @Getter
 public enum TransactionEventType {
     TRANSACTION_FUNDS_REQUESTED(KafkaTopics.TRANSACTION_FUNDS_REQUESTED, "v1"),
-    TRANSACTION_COMPLETED(KafkaTopics.TRANSACTION_COMPLETED, "v1"),
-    TRANSACTION_FAILED(KafkaTopics.TRANSACTION_FAILED, "v1"),
-    TRANSACTION_COMPENSATED(KafkaTopics.TRANSACTION_COMPENSATED, "v1");
+    TRANSACTION_FAILED(KafkaTopics.TRANSACTION_FAILED, "v1");
 
     private final String topic;
     private final String version;
